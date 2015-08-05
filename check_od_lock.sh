c7c918546d23fae0acfe72c3886c1d88a8fe4209
Derekcat <derekcat@gmail.com>
2015-08-05 12:58:05 -0700

    Create check_od_lock.sh

---
+++ check_od_lock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script written by Derek DeMoss (github.com/derekcat) for Dark Horse Comics, Inc. 2015
+
+# Make a list of OpenDirectory Users
+ODUSERS="$(dscl /LDAPv3/127.0.0.1 -list /Users | grep -v vpn | grep -v ldap)"
+# Cleanly initialize our list of disabled users
+DISABLEDUSERS=""
+
+for user in $ODUSERS # Step through our list of users
+do
+	# If they're disabled, then add them to the list
+	YESDISABLED="$(pwpolicy -u $user -getpolicy | awk '{print $1}' | grep isDisabled=1)"
+	if [ $YESDISABLED ]
+	then
+		DISABLEDUSERS+="$user "
+	fi
+done
+
+if [[ -n "$DISABLEDUSERS" ]] #if $DISABLEDUSERS is not empty, print them
+then
+	echo "SERVICE STATUS: Critical"
+	echo "Disabled user[s]:$DISABLEDUSERS"
+	exit 2
+else
+	echo "SERVICE STATUS: OK"
+	exit 0
+fi
